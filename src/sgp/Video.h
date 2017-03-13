c93420cfa9cb249752b4c350fbf2fe7f8722f04e
Momoko Hanasaki <hanasaki.momoko@gmail.com>
2017-03-13 17:08:54 +0100

    Don't include SDL_video.h from Video.h
    Use a forward declaration of the one type needed instead to speed
    up compilation a little.

--- src/sgp/Video.h
+++ src/sgp/Video.h
@@ -1,32 +1,30 @@
 #ifndef VIDEO_H
 #define VIDEO_H
 
-#include <SDL_video.h>
 #include "Types.h"
 
 
 #define VIDEO_NO_CURSOR 0xFFFF
 #define GAME_WINDOW g_game_window
 
+struct SDL_Window;
 extern SDL_Window* g_game_window;
 
-void         VideoSetFullScreen(BOOLEAN enable);
-void         InitializeVideoManager(void);
-void         ShutdownVideoManager(void);
-void         SuspendVideoManager(void);
-void         InvalidateRegion(INT32 iLeft, INT32 iTop, INT32 iRight, INT32 iBottom);
-void         InvalidateScreen(void);
-void         EndFrameBufferRender(void);
-void         PrintScreen(void);
+void VideoSetFullScreen(BOOLEAN enable);
+void InitializeVideoManager(void);
+void ShutdownVideoManager(void);
+void SuspendVideoManager(void);
+void InvalidateRegion(INT32 iLeft, INT32 iTop, INT32 iRight, INT32 iBottom);
+void InvalidateScreen(void);
+void EndFrameBufferRender(void);
+void PrintScreen(void);
 
 /* Toggle between fullscreen and window mode after initialising the video
  * manager */
 void VideoToggleFullScreen(void);
 
 void SetMouseCursorProperties(INT16 sOffsetX, INT16 sOffsetY, UINT16 usCursorHeight, UINT16 usCursorWidth);
-
 void InvalidateRegionEx(INT32 iLeft, INT32 iTop, INT32 iRight, INT32 iBottom);
-
 void RefreshScreen(void);
 
 // Creates a list to contain video Surfaces
