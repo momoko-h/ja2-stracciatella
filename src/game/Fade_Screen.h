7e55288ccc25a0ba1006fed39b6a132a9c722c17
Momoko Hanasaki <hanasaki.momoko@gmail.com>
2026-09-06 09:29:30 +0200

    Clean up Fade_Screen
    • Remove unused (always false) gfFadeInVideo
    • Move declarations from header to source file where possible
    • Remove unused argument of BeginFade()

--- src/game/Fade_Screen.h
+++ src/game/Fade_Screen.h
@@ -4,23 +4,14 @@
 #include "ScreenIDs.h"
 #include "Types.h"
 
-#define FADE_OUT_REALFADE	5
-
-#define FADE_IN_REALFADE	12
-
 typedef void (*FADE_HOOK)( void );
 
 extern FADE_HOOK gFadeInDoneCallback;
 extern FADE_HOOK gFadeOutDoneCallback;
 
 
-typedef void (*FADE_FUNCTION)( void );
-
-
 extern BOOLEAN       gfFadeInitialized;
 extern BOOLEAN       gfFadeIn;
-extern FADE_FUNCTION gFadeFunction;
-extern BOOLEAN       gfFadeInVideo;
 
 BOOLEAN HandleBeginFadeIn(ScreenID uiScreenExit);
 BOOLEAN HandleBeginFadeOut(ScreenID uiScreenExit);
